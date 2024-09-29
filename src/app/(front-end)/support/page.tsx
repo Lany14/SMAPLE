56a518dcef02b7f0b624e20d8478338f14768043
Jhayson Quirao <112755607+jhaysonq@users.noreply.github.com>
2024-09-30 00:23:00 +0800

    refactor: Update import path for Contact component in page.tsx

--- src/app/(front-end)/support/page.tsx
+++ src/app/(front-end)/support/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Contact from "@/components/Contact";
+import Contact from "@/components/FrontEnd/Site/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
