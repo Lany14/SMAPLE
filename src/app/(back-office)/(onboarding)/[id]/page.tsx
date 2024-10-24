1b83765d54c83e5df7dc0b5913ffde8e62ee20b2
Jhayson Quirao <112755607+jhaysonq@users.noreply.github.com>
2024-10-24 22:26:30 +0800

    V1.1.4

---
+++ src/app/(back-office)/(onboarding)/[id]/page.tsx
@@ -0,0 +1,14 @@
+// "use client";
+
+// import React, { useEffect } from "react";
+
+// export const OnboardingPage = () => {
+//   useEffect(() => {
+//     const rootLayout = document.getElementById("RootLayout");
+//     if (rootLayout) {
+//       rootLayout.remove();
+//     }
+//   }, []);
+
+//   return <div>OnboardingPage</div>;
+// };
