338a523642e39b99c22b9018eba9154af95623f2
Jhayson Quirao <112755607+jhaysonq@users.noreply.github.com>
2024-11-09 06:10:43 +0800

    V1.2.2

--- src/types/brand.ts
+++ src/types/brand.ts
@@ -1,7 +1,21 @@
 export type Brand = {
+  visitors: number;
+  revenues: string;
+  sales: number;
+  conversion: number;
+  logo: string;
   id: number;
   name: string;
   href: string;
   image: string;
   imageLight: string;
+};
+
+export type BRAND = {
+  logo: string;
+  name: string;
+  visitors: number;
+  revenues: string;
+  sales: number;
+  conversion: number;
 };
